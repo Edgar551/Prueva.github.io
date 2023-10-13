790931797d2f3f3fc09b66aaeac7a62af2c67fcd
Edgar Eduardo Barreto Benitez <20300006@uttt.edu.mx>
2023-10-13 16:30:57 -0600

    Laravel

---
+++ resources/views/contacto/form.blade.php
@@ -0,0 +1,29 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $contacto->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('direcion') }}
+            {{ Form::text('direcion', $contacto->direcion, ['class' => 'form-control' . ($errors->has('direcion') ? ' is-invalid' : ''), 'placeholder' => 'Direcion']) }}
+            {!! $errors->first('direcion', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('telefono') }}
+            {{ Form::text('telefono', $contacto->telefono, ['class' => 'form-control' . ($errors->has('telefono') ? ' is-invalid' : ''), 'placeholder' => 'Telefono']) }}
+            {!! $errors->first('telefono', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('correo') }}
+            {{ Form::text('correo', $contacto->correo, ['class' => 'form-control' . ($errors->has('correo') ? ' is-invalid' : ''), 'placeholder' => 'Correo']) }}
+            {!! $errors->first('correo', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
